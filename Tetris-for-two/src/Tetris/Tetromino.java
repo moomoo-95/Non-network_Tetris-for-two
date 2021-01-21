80ccd1d6933e903756d6349af7441a0d5d79612f
moomoo-95 <58906637+moomoo-95@users.noreply.github.com>
2021-01-21 23:36:39 +0900

    .

--- Tetris-for-two/src/Tetris/Tetromino.java
+++ Tetris-for-two/src/Tetris/Tetromino.java
@@ -87,3 +87,208 @@ class ITetromino extends Tetromino{
 		return block[R];
 	}
 }
+
+class JTetromino extends Tetromino{
+	public JTetromino() {
+		this.block = new int[][][] {
+			{
+				{0, 1, 0},
+				{0, 1, 0},
+				{1, 1, 0}
+			},
+			{
+				{1, 0, 0},
+				{1, 1, 1},
+				{0, 0, 0}
+			},
+			{
+				{1, 1, 0},
+				{1, 0, 0},
+				{1, 0, 0}
+			},
+			{
+				{1, 1, 1},
+				{0, 0, 1},
+				{0, 0, 0}
+			}
+		};
+		numOfBlockType = 4;
+		X = 4;
+		Y = 0;
+		R = 0;
+		W = 3;
+		H = 3;
+		colorType = 2;
+		
+	}
+	
+	public int[][] getBlock(){
+		return block[R];
+	}
+}
+
+class LTetromino extends Tetromino{
+	public LTetromino() {
+		this.block = new int[][][] {
+			{
+				{1, 0, 0},
+				{1, 0, 0},
+				{1, 1, 0}
+			},
+			{
+				{1, 1, 1},
+				{1, 0, 0},
+				{0, 0, 0}
+			},
+			{
+				{1, 1, 0},
+				{0, 1, 0},
+				{0, 1, 0}
+			},
+			{
+				{0, 0, 1},
+				{1, 1, 1},
+				{0, 0, 0}
+			}
+		};
+		numOfBlockType = 4;
+		X = 4;
+		Y = 0;
+		R = 0;
+		W = 3;
+		H = 3;
+		colorType = 3;
+		
+	}
+	
+	public int[][] getBlock(){
+		return block[R];
+	}
+}
+
+class OTetromino extends Tetromino{
+	public OTetromino() {
+		this.block = new int[][][] {
+			{
+				{1, 1},
+				{1, 1}
+			}
+		};
+		numOfBlockType = 1;
+		X = 4;
+		Y = 0;
+		R = 0;
+		W = 2;
+		H = 2;
+		colorType = 4;
+		
+	}
+	
+	public void rotate() {
+		R = 0;
+	}
+	
+	public void prerotate() {
+		R= 0;
+	}
+	public int[][] getBlock(){
+		return block[R];
+	}
+}
+
+class TTetromino extends Tetromino{
+	public TTetromino() {
+		this.block = new int[][][] {
+			{
+				{1, 1, 1},
+				{0, 1, 0},
+				{0, 0, 0}
+			},
+			{
+				{0, 1, 0},
+				{1, 1, 0},
+				{0, 1, 0}
+			},
+			{
+				{0, 1, 0},
+				{1, 1, 1},
+				{0, 0, 0}
+			},
+			{
+				{1, 0, 0},
+				{1, 1, 0},
+				{1, 0, 0}
+			}
+		};
+		numOfBlockType = 4;
+		X = 4;
+		Y = 0;
+		R = 0;
+		W = 3;
+		H = 3;
+		colorType = 5;
+		
+	}
+	
+	public int[][] getBlock(){
+		return block[R];
+	}
+}
+
+class STetromino extends Tetromino{
+	public STetromino() {
+		this.block = new int[][][] {
+			{
+				{0, 1, 1},
+				{1, 1, 0},
+				{0, 0, 0}
+			},
+			{
+				{1, 0, 0},
+				{1, 1, 0},
+				{0, 1, 0}
+			}
+		};
+		numOfBlockType = 2;
+		X = 4;
+		Y = 0;
+		R = 0;
+		W = 3;
+		H = 3;
+		colorType = 6;
+		
+	}
+	
+	public int[][] getBlock(){
+		return block[R];
+	}
+}
+
+class ZTetromino extends Tetromino{
+	public ZTetromino() {
+		this.block = new int[][][] {
+			{
+				{1, 1, 0},
+				{0, 1, 1},
+				{0, 0, 0}
+			},
+			{
+				{0, 1, 0},
+				{1, 1, 0},
+				{1, 0, 0}
+			}
+		};
+		numOfBlockType = 2;
+		X = 4;
+		Y = 0;
+		R = 0;
+		W = 3;
+		H = 3;
+		colorType = 7;
+		
+	}
+	
+	public int[][] getBlock(){
+		return block[R];
+	}
+}
